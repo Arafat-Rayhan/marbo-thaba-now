83a03c47a81f31e7c29607a68b1063f6d459c5b1
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-20 15:20:40 +0600

    variable

---
+++ app.js
@@ -0,0 +1,19 @@
+const barthDate =2009;
+console.log( barthDate);
+
+const myBrotherSister =2;
+console.log(myBrotherSister);
+
+let student = 1300;
+console.log(student);
+
+const marks ='jayyed juddan';
+console.log( marks);
+
+const room =3;
+console.log(room);
+ 
+const stadyHour =8;
+
+let totel = 48;
+console.log(totel);
---
+++ app2.js
@@ -0,0 +1,19 @@
+const name ='Arafat Rayhan';
+console.log(name);
+
+let address = 'jatrabari dhaka';
+console.log(address);
+
+const myMadrasha = 'jatrabari madania';
+console.log( myMadrasha);
+ 
+const ulama ='taqi usmani';
+console.log( ulama);
+
+let favorateBook ='dalaelun nabooah';
+console.log(favorateBook);
+ 
+const hobby = 'change the world';
+console.log( hobby);
+ let favorateColor ='blue';
+ console.log( favorateColor);
