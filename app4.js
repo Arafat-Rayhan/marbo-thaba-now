960053539ed45b65cffb8c6db8dc8e45cdad7049
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-20 18:29:49 +0600

    math

---
+++ app4.js
@@ -0,0 +1,57 @@
+let earnMony =100;
+let use =40;
+let remaining =(earnMony-use);
+console.log( remaining);
+
+const havePancil =10;
+const useEveryTime =2;
+const totelUseTarm =(havePancil/useEveryTime);
+console.log(totelUseTarm);
+
+let haveMony = 60;
+let everyTimeUse =15;
+let totelChanc =( haveMony/everyTimeUse);
+let remainAfter =(haveMony%everyTimeUse);
+console.log(totelChanc);
+console.log(remainAfter);
+
+let hi ='Hello';
+let so ='word';
+let mean =(hi+so);
+console.log(mean);
+
+let riceKg =60;
+let mangoKg =30;
+let totelMass =(riceKg+mangoKg);
+console.log(totelMass);
+
+let rayhanHave =500;
+let useOnAllTime =75;
+//let chance =(rayhanHave/useAllTime);
+let realNum =500-(rayhanHave%useOnAllTime);
+let chance =realNum/75;
+
+let noUse =(rayhanHave%useOnAllTime);
+console.log(chance);
+console.log(noUse);
+
+let num1 =8;
+let num2 =3;
+console.log(num1%num2);
+let stong =8-(num1%num2);
+console.log(stong);
+
+let num3 =50;
+let num4 =9;
+console.log(num3%num4);
+ 
+let half1 ='Bangla'
+let half2 ='desh';
+console.log(half1+half2);
+
+let foodKg =480;
+let everyDay =4;
+let lasting =(foodKg/everyDay);
+let forMonth =(foodKg/everyDay)/30;
+console.log(lasting);
+console.log(forMonth);
