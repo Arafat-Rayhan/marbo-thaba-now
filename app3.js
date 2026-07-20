295553c9ef40977467a6a20c3884a9e419c22a82
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-20 15:40:35 +0600

    bolian

---
+++ app3.js
@@ -0,0 +1,17 @@
+const isHappy = false;
+console.log(isHappy);
+
+const haveGirlFriend =false;
+console.log(haveGirlFriend);
+
+const amHardWorking =true;
+console.log(amHardWorking);
+
+let isStudyDone = false;
+console.log(isStudyDone);
+
+let phoneWillchanged = true;
+console.log( phoneWillchanged);
+ 
+const isBracFristDone = true;
+console.log(isBracFristDone);
