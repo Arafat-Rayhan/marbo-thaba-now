d5a46991390c1d7fd58b63c20392c7f66d9ab87a
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-21 23:03:29 +0600

    let use

---
+++ chapter3/let.js
@@ -0,0 +1,28 @@
+let age = 19;
+age =12;
+console.log(age);
+
+let poketMony =500;
+poketMony =15;
+console.log( poketMony);
+
+let studyTime =4;
+studyTime=5;
+console.log( studyTime);
+
+let myFriend = 16;
+myFriend=5;
+console.log(myFriend);
+
+let phonePrice =10000;
+phonePrice=6000;
+console.log(phonePrice);
+
+let chairCount =45;
+chairCount=35;
+chairCount=40;
+console.log(chairCount);
+
+let playTime =3;
+playTime=0.2;
+console.log(playTime);
