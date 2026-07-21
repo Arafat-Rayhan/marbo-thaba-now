934c7d8f1ea7fa8b2f8c26863352f1a0d9126423
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-21 22:39:29 +0600

    typeOf

--- chapter3/type.js
+++ chapter3/type.js
@@ -1,2 +1,16 @@
 const price = 700;
 console.log(typeof price);
+let learning =true;
+console.log( typeof learning);
+
+let megaCity ='Dhaka';
+console.log(typeof megaCity);
+
+let temperature = 102.4;
+console.log(typeof temperature);
+
+let isHungry = true;
+console.log(typeof isHungry);
+
+let isRich = true;
+console.log( typeof isRich);
