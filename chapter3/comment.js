b76f89c9822b2298f010e6e163448ed1fa3ca7ef
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-22 14:22:21 +0600

    comment

---
+++ chapter3/comment.js
@@ -0,0 +1,26 @@
+let scure= 98;
+//coding scure
+ 
+let radius = 37;
+//corderd radius
+
+let myFavorateDarss=4;
+/*myFavorateDarss= white , green ,*/
+
+let giftMony= 100;
+//mijan gordan
+
+let studyTime=9;
+/*for 2 hour code
+for 2 hour code
+for 2hour code
+for 1 hour code
+for 1 hour revaice and analice */
+
+let pyment =5500;
+//for programming hero
+//to learn programming
+
+let onlinePay =156;
+//for book=150
+//for dalivari
