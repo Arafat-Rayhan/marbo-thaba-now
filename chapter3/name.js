fd999c1b56b4b5d564defff3f4619d1677add4be
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-22 14:52:44 +0600

    name roul

---
+++ chapter3/name.js
@@ -0,0 +1,21 @@
+let meetOfCow ='my favorate ';
+
+const city ='dhaka';
+const City ='kabul'
+console.log(city);
+console.log( City);
+
+let userName=rayhan;
+let username = arafat;
+
+let myHous ='danea';
+let myhous ='majar gile'
+
+// let super=38;
+// console.log(super);
+
+// let class=11;
+// console.log(class);
+
+// let 2ndPosition='tui';
+// console.log(2ndPosition);
