446e85c096413144c6fadfeca73bb611744c3cfc
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-24 15:25:53 +0600

    parse and Nan

---
+++ chapter3/parse.js
@@ -0,0 +1,61 @@
+//semple
+const sugar = parseInt('20')
+console.log( sugar  );
+
+const fotherAge = '50';
+const fotherAgeNum= parseInt(fotherAge);
+console.log( fotherAgeNum);
+
+// when the letter is come
+const input='a89';
+const input2='55r6';
+const outcome =parseInt(input);
+const outcome2 =parseInt( input2);
+console.log(outcome2,outcome);
+//the is broken
+const height = '161.44';
+const heightNum =parseInt(height);
+console.log( heightNum);
+// full
+const heightR = parseFloat(height);
+console.log(heightR);
+//to see after dasimall 
+let point = 2.632.toFixed(1);
+console.log(point);
+console.log(typeof point
+);
+console.log(parseFloat(point));
+
+// start to practice
+
+let num='20';
+let numRight=parseInt(num);
+console.log(numRight);
+console.log(numRight+10);
+
+let iHave =parseInt('3.14159');
+console.log(iHave);
+// it is fall
+// const practice=primikBoy;
+// console.log(practice);
+
+let counting=(3.456+2.789).toFixed(2);
+console.log(counting);
+
+let found1=('56.78');
+let found2=('12.34');
+let result =parseFloat(found1)+parseFloat(found2);
+console.log(result);
+
+
+let weFound1 = 10.5678;
+let weFound2 = 5.432;
+let weResult1 =weFound1.toFixed(1);
+let weResult2 =weFound2.toFixed(1);
+console.log(weResult1,weResult2);
+
+
+const last='42.45689754';
+const lastN=parseInt(last);
+const lastL=lastN.toFixed(3);
+console.log(lastL);
