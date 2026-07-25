961994f7a6a37f9ace26fc5b8f13a91604fbe64a
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-25 12:20:18 +0600

    indax

---
+++ chapter6/indax.js
@@ -0,0 +1,26 @@
+const myFriend =['Arafat','Rayhan','Arabic','staudy','hardwork'];
+const friend3 =myFriend[3];
+console.log(friend3);
+
+
+
+const myFavorateBook=['bokhari','muslim','nasaye','ibn mazah','abodaod','tirmeze','moatta malik'];
+console.log(myFavorateBook[4]);
+
+const num =[1,2,3,4,5,6,7,8,9,10];
+num[7]=30;
+console.log(num);
+
+const app =['vs code','linkdin','discord','chat gpt'];
+app[1]='github';
+console.log(app);
+
+const myFammaly =['fother','mother','brother','sister','rayhan'];
+console.log(myFammaly[2]);
+
+const dreem = ['dawat','safar','moeony','solve','juhad'];
+console.log(dreem[3]);
+
+//this is an error
+// const myTable =['book','laptoop','pen'];
+// console.log(myTable[5]);
