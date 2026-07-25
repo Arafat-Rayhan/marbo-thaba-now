ff7b19e9357bbf575611a63daf7bf5044460c71c
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-25 10:53:06 +0600

    array and length

---
+++ chapter6/array.js
@@ -0,0 +1,19 @@
+const num71to79 =[71.72,73,74,75,76,77,78,79];
+
+const fruts = ['apple','mango','bnana','jackfrut','lichi'];
+
+const myBooksalf=['quran','hadith','tarikh','osul','english','programm','webdevelopment','helth','food','analyses',];
+const numOfBook=myBooksalf.length;
+console.log(numOfBook);
+
+const favorateBook=['tocknowledge','socmarket','kiptro','syver','selfimprove'];
+
+const oddNum =[11,13,15,17,19,21,23,25,27,29];
+
+const color=['white','yello','black','green','pink'];
+const colorn=color.length;
+console.log(colorn);
+
+const capotals =['dhaka','kabul','charachi','gaja','makka','madina'];
+const capitalNum =capotals.length;
+console.log(capitalNum);
