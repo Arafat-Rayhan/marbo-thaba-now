5d3846173ea81fa3347dc37faf3d105cc390ace1
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-25 16:26:35 +0600

    includes-index

---
+++ chapter6/include.js
@@ -0,0 +1,26 @@
+//include
+const usol =['sahih','motlac','moshtarak','mutashbih'];
+console.log(usol.includes('mutashbih'));
+//2nd
+console.log(usol.includes('nash'));
+//indexof
+const sister=['nila','somayya','shadeaa'];
+console.log(sister.indexOf('shadeaa'));
+
+//practice
+
+const fruts =['mango','lichi','bananah'];
+console.log(fruts.indexOf('mango'));
+
+const name= ['babul','alif','choton'];
+console.log(name.indexOf('babul'));
+
+const friend =['rimon','rifat','rajib'];
+console.log(friend.indexOf('rifat'));
+
+const city = ['Dhaka','Chettagong','Sylet','jatrabari'];
+console.log(city.includes('Jatrabri'));
+
+const sports =['football','cricate','kabadi'];
+console.log(sports.includes('voliball'));
+
