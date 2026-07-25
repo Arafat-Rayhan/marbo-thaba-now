a7abf872a808ee35c6bca012ba405b37736dab98
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-25 15:28:48 +0600

    push-pop

---
+++ chapter6/push-pop.js
@@ -0,0 +1,65 @@
+//push
+const number =[13,78,98,45];
+number.push(44);
+console.log(number);
+
+//pop
+const books =['bangla','hadith','english','urdo'];
+books.pop();
+console.log(books);
+
+//extra------push return the new length not array
+const student=[1,5,7,23,67];
+const theBest=student.push(15);
+console.log(student);
+console.log( theBest);
+
+//extra
+const ingneer =['web','soft ware','hous'];
+const theLess =ingneer.pop();
+console.log( theLess);
+
+
+//shift
+
+const fruts=['apple','liche','jam','banana'];
+fruts.shift();
+console.log(fruts);
+ //unshift
+ const bad =['batpr','chiter','dandabaj'];
+ bad.unshift('chor');
+ console.log(bad);
+
+
+
+
+ //practis
+
+
+ const nunber =[10,20,30,40,50];
+ number.push(60);
+ console.log(number);
+
+
+ const kitab =['bokhari','muslim','nasayee','abu dawod'];
+ kitab.push('ibn mazah');
+ console.log(kitab);
+
+
+ const laptoop =['hp','lenovo','dell','asua']
+ laptoop.pop();
+ console.log(laptoop);
+
+
+ const multipulictio =[24,36,48,60,];
+ multipulictio.unshift(12);
+ console.log(multipulictio);
+
+
+ const programming =['html','css','javascript','python']
+ programming.shift();
+ console.log(programming);
+ 
+
+
+
