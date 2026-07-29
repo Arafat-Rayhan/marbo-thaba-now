c69292602b67dbca708702903f9c98ab487ddc27
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-29 12:25:52 +0600

    equal==and ! nad ===

---
+++ chapter4/equal.js
@@ -0,0 +1,29 @@
+const iHave =50;
+const youHave ='50';
+console.log(iHave==youHave);
+console.log(iHave===youHave);
+
+const inJSnode ='JavaScript';
+const inBook ='JavaScript';
+console.log(inJSnode==inBook);//every one is equle
+
+const bookPrice =35;
+const padePrice ='35';
+console.log(bookPrice==padePrice);//not count string
+console.log(bookPrice===padePrice);//count string
+
+console.log('apple'=='apple');
+console.log('apple'==='apple');
+
+console.log('test'=='TEST');//not work
+console.log('test'==='TEST');//not work
+
+const x =15;
+const y ='20';
+console.log(x!=y);
+console.log(x!==y);// this is difficul
+
+const A ='hello';
+const B ='Hello';
+console.log(A==B);
+console.log(A===B);
