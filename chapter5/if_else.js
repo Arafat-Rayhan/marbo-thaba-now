7a2980fea4f918a6b359bc13147d809a346177bc
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-29 15:26:31 +0600

    if and else

---
+++ chapter5/if_else.js
@@ -0,0 +1,64 @@
+//basic
+if(3<10){
+    console.log(' i am small.i do not bite');
+}
+// if bocome false the code not work
+if(13<10){
+    console.log('smaller but Storager');
+}
+//if and else
+
+  const becomeWebdevloperrue=false;
+  if(becomeWebdevloperrue==true){
+    console.log('i well do all');
+  } else{
+        console.log('do heard work');
+    }
+
+    const weight =50;
+    if(weight<56){
+        console.log('ride on the car');
+    }else{
+        console.log('walk is best' );
+    }
+// practice
+
+const arrivingTime= 10;
+if(arrivingTime<=9){
+    console.log('you are well');
+}else{
+    console.log('i will panish you');
+}
+
+const isLoging = true;
+if(isLoging==true){
+    console.log('welcome');
+}else{
+    console.log('chake your data');
+}
+
+const isRuning =5;
+if(isRuning>=5){
+    console.log('i will give you choclet');
+}else{
+    console.log('you will become fat');
+}
+
+const marks = 85 ;
+if(marks>=80){
+    console.log('got the bic');
+}else{
+    console.log('weat the next exeam');
+}
+
+const money = 34000;
+if(money>=50000){
+    console.log('ready to marred');
+}else{
+    console.log("don't weast time");
+}
+
+const temperature = 25;
+if(temperature>=23){
+    console.log('on the fan ');
+}
