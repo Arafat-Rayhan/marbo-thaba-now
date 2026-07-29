47a809996fd5930a561f06ac01d106212655e8ff
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-29 11:54:41 +0600

    comparicson with variable

---
+++ chapter4/variableC.js
@@ -0,0 +1,32 @@
+const muajeAge =14;
+const rayhanAge =19;
+console.log(rayhanAge>muajeAge);
+
+const myNumber =45;
+const passmark =33;
+console.log(myNumber>=passmark);
+
+const jambora =49;
+const kadbal =34;
+console.log('is cheep',jambora>kadbal);
+
+const iRead =10;
+const youRead =10;
+console.log('we are equal',iRead==youRead);
+
+const myCacke =15;
+const friendCacke =10;
+console.log('hi have more then i',15>10);
+
+const batparfifa =47;
+const batparicc =6;
+console.log('big batpar',batparfifa>batparicc);
+
+const myName ='rayhan';
+const yuorName ='karim';
+console.log(myName==yuorName);
+
+const iStudy =true;
+const youStudy=false;
+console.log(iStudy==youStudy);
+
