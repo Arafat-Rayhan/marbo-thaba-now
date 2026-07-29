4158cb2110d06438edd59037d7c0a447fc8b5ad9
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-30 00:01:59 +0600

    and or

---
+++ chapter5/and_or.js
@@ -0,0 +1,89 @@
+//&&
+const salary =45000;
+const isProgrammer = true;
+if(salary==50000 && isProgrammer== true){
+    console.log('good man');
+
+}else{
+    console.log('get heard work');
+}
+//or||
+ 
+const earn = 25000;
+const heigh = 72;
+if(earn<=2000 || heigh>=60){
+    console.log('see our duaghter');
+}else{
+    console.log('we are not agary');''
+}
+//practice
+ 
+const age =18;
+const height =80;
+if(age<=18 && height<=60){
+    console.log('sit dwon on the car');
+}else{
+    console.log('drive the car');
+}
+
+const marksMath =85;
+const marksEnglish =93;
+if(marksMath>=85 && marksEnglish>=80){
+    console.log('door is open');
+}else{
+    console.log('go to anather');
+}
+
+const haveGPA=true;
+const gdpOfFammaly=10000;
+if(haveGPA==true && gdpOfFammaly<=10000){
+    console.log('get sclorship');
+}else{
+    console.log('you are on fee');
+}
+const jobSeekerAge = 30;
+const jobSeekerExprence = 2;
+if(jobSeekerAge<=30 && jobSeekerExprence>=2){
+    console.log('get intervwe');
+}else{
+    console.log('you are not elegeble');
+}
+ 
+const egg = 11;
+const chicken = false;
+if(egg>=12 || chicken ==false){
+    console.log('egg recipy');
+}else{
+    console.log('bread with banana');
+}
+const tempatire =99;
+const kashi = true;
+if(tempatire>=100 && kashi==true){
+    console.log('go to doctor');
+}else{
+    console.log('sleep into qualt');
+}
+
+const pMarks = 84;
+const homeWork = true;
+if(pMarks>=80 && homeWork== true){
+    console.log('get the exeam');
+}else{
+    console.log('Auto fale');
+}
+
+const electricity = false;
+const phonCharge = false;
+if(electricity==false && phonCharge==false){
+    console.log('go to desk');
+}else{
+    console.log('go to code');
+}
+
+const shartPrice=1000;
+const copon = true;
+if(shartPrice==1000 && copon==true){
+    console.log('have discount');
+}else{
+    console.log('39 tk off');
+}
