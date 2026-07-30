a3a1cfe9b7dee67906c5e49f3ba535f6997a49a2
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-30 12:05:02 +0600

    else if

---
+++ chapter5/else_if.js
@@ -0,0 +1,88 @@
+const price = 6500;
+if(price>=5000){
+    const discount = price/100*10;
+    const pay = price - discount;
+    console.log(pay);
+}else{
+    console.log(price);
+}
+//else if
+
+const money=4000;
+if(money>=5000){
+    const discountMoney =money/ 100 *10;
+    const payMoney = money - discountMoney;
+    console.log(payMoney);
+}else if(money>=2500){
+    const discountMoney = money/100 *5;
+    const payMoney = money - discountMoney;
+    console.log(payMoney);
+}else{
+    console.log(money);
+}
+//practice
+
+const bay = 4500;
+if(bay>=3000 ){
+    const discount= bay/100*5;
+    const pay = bay-discount;
+    console.log(pay);
+}else if(bay>=6000){
+    const discount = bay / 100 *10;
+    const pay = bay - discount;
+    console.log(pay);
+}
+
+let age = 32;
+if(age<=12 || age>=60){
+    console.log('free');
+}else{
+    console.log('must pement');
+}
+
+let balance = 4677;
+if(balance<1000){
+    console.log('put dipogit');
+}else if(balance<=5000){
+    console.log('get injoy');
+}else if(balance>5000){
+    console.log('rady to marrad');
+}else{
+    console.log('get heard work');
+}
+
+let marks = 53;
+if(marks<50){
+    console.log('fail');
+}else if(marks<80){
+    console.log('pass');
+}else{
+    console.log('A+');
+}
+ 
+const page = 746;
+if(page<=100){
+    console.log('small book');
+}else if(page>=500){
+    console.log('lerg size book');
+}else{
+    console.log('big book');
+}
+
+const tempariture = 3;
+if(tempariture<=0){
+    console.log('ice');
+}else if(tempariture<=20){
+    console.log('cool cool');
+}else{
+    console.log('hot hot');
+}
+
+const gameLevel=7;
+if(gameLevel<10){
+    console.log('nice');
+}else if(gameLevel<=50){
+    console.log('expert');
+}else if(gameLevel>50){
+    console.log(object);
+}
