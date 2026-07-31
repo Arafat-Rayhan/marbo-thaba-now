6a859184534af72a0ac28544fd8b33bdb9989da1
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-31 18:08:38 +0600

    for of

---
+++ chapter7/loop_for_of.js
@@ -0,0 +1,46 @@
+//for of
+const numbers =[12,98,45,63,21,72];
+for(const num of numbers){
+    console.log(num);
+}
+
+// string
+
+const fruits =['apple','orange','banana','jackfruit','watermelon'];
+
+for(const fruit of fruits){
+    console.log(fruit);
+}
+
+//practice
+
+const subjects =['language','hadith','dawah','programmein','stoc market'];
+
+for(const subject of subjects){
+    console.log(subject);
+}
+
+const foods = [ 'nut','hunny','dates','chiskpeas','egg'];
+
+for(const food of foods){
+    console.log(food);
+}
+
+const years =[1971,1985,2006,2009,2013,];
+
+for(const year of years){
+    console.log(year);
+}
+
+const jamat =['mojahid','jihad','mutala','writing','programming'];
+
+for(const fard of jamat){
+    console.log(fard);
+}
+
+
+const have =['kitab','laptop','tws','english book','tarikh'];
+
+for(const item of have){
+    console.log(item);
+}
