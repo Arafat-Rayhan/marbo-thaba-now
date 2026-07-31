6d64362684f66d70868d1027c7eaf2cfd376ab1e
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-31 21:30:16 +0600

    whil

---
+++ chapter7/while.js
@@ -0,0 +1,43 @@
+// exampol
+let num = 0;
+while(num<5){
+    console.log(num);
+    num = num +2;
+}
+
+let goSchool = 0;
+while (goSchool<5){
+    console.log('a k school');
+     goSchool++;
+}
+//practice
+let code = 0;
+while(code<10){
+    console.log('every day i code 3 hour');
+    code++;
+}
+let roll = 1;
+while(roll<=20){
+ console.log(roll);
+ roll++;
+}
+
+let misson = 50;
+while(misson<=100){
+    console.log(misson);
+    misson++;
+}
+
+let button = 5;
+let i = 1;
+while(i<=10){
+    console.log(button + 'x' + i +"="+(button*i));
+    i++;
+
+}
+
+let car = 110;
+while(car<=111){
+    console.log(car/2);
+    car++;
+}
