7a928180257e89a16b16b78499d2c5f914a25404
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-31 06:57:50 +0600

    ternary

---
+++ chapter5/ternary.js
@@ -0,0 +1,33 @@
+//
+const age = 18;
+age>=18 ? console.log('you are vooster'):console.log('you are children');
+
+let price = 600;
+let isLeader = true;
+price = isLeader === true? 0:price + 100;
+console.log(price);
+
+//practice
+
+let payBill = 3258;
+let cashBack = 762;
+cashBack = payBill >=3000?500: 0;
+console.log(cashBack);
+
+let myAge = 15;
+myAge = myAge>15? console.log('Teenager'):console.log('child');
+
+let isloging = true;
+isloging===true?console.log('wellcome back'):console.log('pless loging');
+
+let fuelTank = 'full';
+fuelTank==='full'?console.log('go to jerny'):console.log('fill the tank');
+
+let isPassed =true;
+isPassed===true?console.log('very good'):console.log('come back'); 
+
+const isSunny = true;
+isSunny===true?console.log('take the umbrallah'):console.log('go go go');
+
+const isEcpensive = true;
+isEcpensive===true?console.log('no baying'):console.log('bay much');
