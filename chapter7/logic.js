0d86eb7801c8d24b7e3662efdc6228e24626e9eb
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-07-31 22:03:17 +0600

    logic

---
+++ chapter7/logic.js
@@ -0,0 +1,49 @@
+//
+let num = 1;
+let sum = 0;
+while(num<=10){
+    console.log(num);
+    sum = sum + num;
+    num++;
+}
+ console.log(sum);
+ 
+ //practice
+ let finger =51;
+ while(finger<=100){
+    console.log(finger);
+    finger++;
+ }
+
+ let phone = 1;
+ let plass =0;
+ while(phone<=15){
+    plass = plass + phone;
+    phone++;
+ }
+ console.log(plass);
+
+
+ let student = 1;
+ let count = 0;
+ while(student<=50){
+    count = count + student;
+    student++;
+ }
+ console.log(count);
+
+ let book = 21;
+ let bcount = 0;
+ while(book<=50){
+    bcount=bcount+book;
+    book++;
+    console.log(bcount);
+ }
+
+ let hadith =20;
+ let hcount = 0;
+ while(hadith<=40){
+    hcount = hcount+hadith;
+    hadith++;
+ }
+console.log(hcount);
