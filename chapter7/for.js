91d5e335675b239ce88d1812f95b83fc9653c662
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-01 22:28:45 +0600

    for

---
+++ chapter7/for.js
@@ -0,0 +1,38 @@
+//start
+for(let num = 0; num<5;num++){
+    console.log(num);
+}
+
+for(let i = 0; i<10; i++){
+    console.log(i);
+}
+
+for(let apple = 50; apple<100; apple++){
+    console.log(apple);
+}
+
+//sum
+
+sum = 0;
+for(let banana = 11;banana<=20;banana++ ){
+    sum = sum + banana;
+}
+console.log('sum of 11 to 20',sum);
+
+//practice
+
+for( let book = 150;book<=170; book++){
+    console.log(book);
+}
+let pless = 0;
+for(let student =31;student<=58;student++){
+    pless = pless + student;
+    console.log(student);
+}
+console.log(pless);
+
+let add = 0;
+for(let tafsir =25; tafsir<=75; tafsir++){
+    add = add + tafsir;
+}
+console.log(add);
