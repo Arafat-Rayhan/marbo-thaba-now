887384abc4ae13d21b533cc0111d4741ac6b051b
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-02 13:23:23 +0600

    object

---
+++ chapter8/object.js
@@ -0,0 +1,109 @@
+//object
+const student = {
+    name:'Arafat Rayhan',
+    age:20,
+    jamat:'dawra',
+    earn:false
+}
+console.log(student);
+//other
+const phone ={
+    brand:'comola',
+    price:5000,
+    color:'asmani',
+    virson:2026
+}
+console.log(phone);
+
+const juce ={
+    name:'karala juce',
+    price:40,
+    sugar:false
+}
+console.log(juce);
+
+const car ={
+    brand:'RAARA',
+    wheels:8,
+    maxSpeed:100,
+    wind:true
+}
+console.log(car);
+
+// differant
+
+const subject ={
+    name:'Biology',
+    teacher:'Anwaer',
+    exanDate:'30 dec',
+    chapter:['first','forth','sixth'],
+    nextExam:{
+        name:'Final',
+        marks:60
+    }
+}
+console.log(subject);
+
+//practic
+const teacher = {
+    name:'jamal',
+    subject:'DAS',
+    age:36,
+    from:'nepal',
+    hight:'59 enc'
+}
+console.log(teacher);
+
+const tree ={
+    name:'mango tree',
+    height:'70 fit',
+    age:25,
+    benefits:['sweet','much fruts','well need'],
+
+}
+console.log(tree);
+
+const cow ={
+    formet:'domesticated',
+    lag:4,
+    arms:'horn',
+    food:'gras',
+    behatbore:'frindly',
+    meets:true
+}
+console.log(cow);
+
+const fother ={
+    name: 'M.A.Mannan',
+    proffeson:'manager',
+    child:3,
+    age:57
+}
+console.log(fother);
+ 
+const motorBike ={
+    brand:'ooarr',
+    wheels:4,
+    hiSpeed:'150+',
+    price:150000
+}
+console.log(motorBike);
+
+const bird ={
+    name:'sparow',
+    speed:'180/s',
+    color:'brown and black and gra',
+    home:'Bangladesh'
+}
+console.log(bird);
+
+const Laptop ={
+    brand:'DELL',
+    prosesor:{
+        name:'intel',
+        core:'i7',
+    },
+    color:'silver',
+
+}
+console.log(Laptop);
