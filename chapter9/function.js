4ad15a37d5d3db6019c84a93979ee9edff5fc081
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-04 08:56:57 +0600

    function

---
+++ chapter9/function.js
@@ -0,0 +1,47 @@
+//function declaretion
+function myName(){
+    console.log('Rayhan');
+}
+myName()
+myName()
+
+// practice
+ function juceMachine(){
+    console.log('your juce is ready');
+ }
+ juceMachine();
+
+ function myRotin(){
+    console.log('code and namej and eat and sleep');
+ }
+ myRotin();
+
+ function myFamily(){
+    console.log('ma , baba, vai , bon , ami');
+ }
+ myFamily();
+
+ function taskDone(){
+    console.log('namaj, code, eat, buth,code');
+ }
+ taskDone();
+
+ function greetMe(){
+    console.log('assalamualikum');
+ }
+ greetMe()
+ 
+ function getFavFrient(){
+    console.log('salman , jahin, mtaaki, mislim, nasir');
+ }
+ getFavFrient();
+
+ function introduceMyself(){
+    const i ={
+        name:' Rayhan',
+        age:20,
+        class:' dawra'
+    }
+    console.log(i);
+ }
+ introduceMyself()
