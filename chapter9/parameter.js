53e44cbcf1ee35287f0f1b93fd066cecb3c0e69a
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-04 12:58:22 +0600

    simple function

---
+++ chapter9/parameter.js
@@ -0,0 +1,77 @@
+//doubled
+function doubled(number){
+    const double = number * 2;
+    console.log(double);
+};
+doubled(3);
+
+// sqare
+ function sqare(number){
+     const sq = number * number;
+     console.log(sq);
+}
+sqare(12)
+
+//sum
+function add(number1,number2){
+    const sum = number1 + number2;
+    console.log(sum);
+}
+add(34,32);
+
+//practice
+
+function fs(fother,son){
+    const sum = fother + son;
+    console.log(sum);
+}
+fs(20,57);
+
+function multily(num1,num2){
+    const calsulate = num1 * num2;
+    console.log(calsulate);
+}
+multily(34,90);
+
+function resultAdd(x,y,z){
+    const add = x + y + z;
+    console.log(add);
+}
+resultAdd(78,90,56);
+
+function age(birth,now){
+    const yourage = now - birth;
+    console.log(yourage);
+}
+age(2004,2026);
+
+function markating(have,price){
+    const bay = have / price;
+    console.log(bay);
+}
+markating(200,35);
+
+function avarage(x,y,z,a){
+    const result =(x+y+z+a)/4;
+    console.log(result);
+}
+avarage(45,57,79,50);
+
+function baying(sealing){
+    const bay =sealing +250 ;
+    console.log(bay);
+}
+baying(3035);
+
+function hundredAge(birth){
+    const your100 = birth + 100;
+    console.log(your100);
+}
+hundredAge(2004);
+ 
+function phoneUse(hours){
+    const inMounth = hours*30;
+    console.log(inMounth);
+}
+phoneUse(3);
+
