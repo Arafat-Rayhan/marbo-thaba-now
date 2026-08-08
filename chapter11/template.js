7844e03241651ec2802a9f2054b4eb550bb685aa
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-08 13:01:57 +0600

    use tenplate

---
+++ chapter11/template.js
@@ -0,0 +1,42 @@
+//practice
+const a = 9;
+const b = 6;
+const result = `the diffference between ${a} and ${b} is ${a-b}`;
+console.log(result);
+
+const employee = {
+    name : 'Arafat Rayhan',
+    age : 22 ,
+    salary : 50000
+}
+const My = `name : ${employee.name} and age : ${employee.age} and salary : ${employee.salary}`;
+console.log(My);
+
+const fruits = ['apple','banana', 'mango', 'vagitable'];
+const myFavorate = `my favorate fruit is ${fruits[2]} `;
+console.log(myFavorate);
+
+const x = 50;
+const y = 10;
+const resultXY= ` The divition of ${x} and ${b} is ${x / y}`;
+console.log(resultXY);
+
+const person = {
+    Firstname : 'Amit',
+    Lastneme:'shah'
+}
+const fullName = ` the full name is ${person.Firstname + person.Lastneme}`;
+console.log(fullName);
+
+const animals =['dog','cow','buird','cat',]
+const favorateAnimal = ` my favorate animale is ${animals[1]}.`;
+console.log(favorateAnimal);
+
+const student = {
+    name :'Arafat',
+    age: 22,
+    result:[77,97,56]
+}
+const studentId = ` the student name is ${student.name} and age ${student.age}
+and avarage mark ${(student.result[0]+student.result[1]+student.result[2])/ 3}`
+console.log(studentId);
