b2c76c7477a7ba19c743b11eba79c5d00fac35c6
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-10 05:59:07 +0600

    reverse

---
+++ chapter13/reverse.js
@@ -0,0 +1,13 @@
+//practice
+const names = ['zara','Tariq','Amir','Lina'];
+names.reverse();
+console.log(names);
+
+const numbers =[1,9,7,4];
+numbers.reverse();
+console.log(numbers);
+
+const user =[{name : 'Ali', age : 30 ,},{name : 'Sara', age : 22,},{name : 'Tariq', age : 35}];
+user.reverse();
+console.log(user);
+
