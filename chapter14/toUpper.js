b90053e846eea7eeda8f3c5efb9cec6b0783f57d
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-10 07:50:33 +0600

    upper cass

---
+++ chapter14/toUpper.js
@@ -0,0 +1,12 @@
+//practice
+const email = 'User@Example.com'
+console.log(email.toLocaleLowerCase());
+
+const greeting ='hELlo WoRLd';
+console.log(greeting.toUpperCase());
+
+const language = 'JavaScript';
+console.log(language.toLocaleLowerCase().includes('script'));
+
+const text = 'Node.js';
+console.log(text.includes('N'));
