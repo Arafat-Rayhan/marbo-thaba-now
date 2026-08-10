d0f24b3c7d5d11eeb855ce09b381869bddec97ea
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-10 16:29:31 +0600

    splite join

---
+++ chapter14/splite-join.js
@@ -0,0 +1,39 @@
+//practice 
+const myFriends="Raju,Rana,Hasan,Kabir,Mahi,Oli"
+const myFriendsArray = myFriends.split(',');
+console.log(myFriendsArray);
+
+const name =['arafat','tanvir','salman','rayhan','yosof'];
+const allName =name.join(',');
+console.log(allName);
+
+const syntax = 'function if else while';
+const syntaxArray = syntax.split(',');
+console.log(syntaxArray);
+
+const languages = ['JavaScript','python','Java'];
+const languageInProgramming =languages.join(';');
+console.log(languageInProgramming);
+
+const loops ='for,whil,fro-in,for-of,do-while';
+const loop =loops.split(',');
+console.log(loop);
+
+const clg ='    consol log debug';
+const Eclg =clg.trim();
+console.log(Eclg);
+
+const srt1 = 'push';
+const srt2 ='pop';
+const srt3 ='shift';
+const srt4 ='unshift';
+const all = srt1.concat(srt1,srt2,srt3,srt4);
+console.log(all);
+
+const spich ='Hello JavaScript Developer';
+const languageInDev =spich.slice(6,15);
+console.log(languageInDev);
+
+const youTalk = 'code more learn more';
+const sWord = youTalk.substring(10,15);
+console.log(sWord);
