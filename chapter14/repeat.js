8aa9f86dca860e90f13b91bbbe9d8c8a7b6940b9
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-10 20:21:18 +0600

    repeat and replace

---
+++ chapter14/repeat.js
@@ -0,0 +1,14 @@
+//practice 
+const yousay ='Hi';
+const again = yousay.repeat(10);
+console.log(again);
+
+// to string 
+
+const iWant ='i am learning JavaScript . JavaScript is ammzaing';
+const now = iWant.replaceAll('JavaScript','Js');
+console.log(now);
+
+const youWant ='I love apple and apple are tasty!';
+const nowU = youWant.replaceAll('apple','mango')
+console.log(nowU);
