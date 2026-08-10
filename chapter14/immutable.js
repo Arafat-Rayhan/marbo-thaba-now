94ad5924bcd0cca6fb2cfa6ef37f08b7e387e086
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-10 07:20:26 +0600

    immutable

---
+++ chapter14/immutable.js
@@ -0,0 +1,14 @@
+//practce 
+const city ='Chattogram';
+console.log(city[6]);
+
+const division = 'Sylhet';
+console.log(division[1]);
+
+const name = 'Rafi'
+console.log(name[3]);
+
+const text = 'Immutable';
+console.log(text.length);
+text[3] = 'n';
+//console.log(text); yeh this is immutable
