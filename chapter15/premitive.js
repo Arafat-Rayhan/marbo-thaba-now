d4557b63ac368789eb7656baabdf00ed9a313aa6
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-13 11:12:48 +0600

    premitive

---
+++ chapter15/premitive.js
@@ -0,0 +1,20 @@
+const num = 34;
+let copy = num;
+copy = 25;
+console.log(copy);
+console.log(num);
+
+let arr =[1,2,3];
+let array = arr;
+array.push(88);
+console.log(arr);
+console.log(array);
+
+let obj ={
+    name : 'Arafat Rayhan',
+    age : 33,
+}
+let x = obj;
+x . class = 'dowra'
+
+console.log(obj,x);
