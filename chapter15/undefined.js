8057219609273d448418cb32e479de79a88570fe
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-13 11:38:27 +0600

    undefined

---
+++ chapter15/undefined.js
@@ -0,0 +1,24 @@
+let name;
+console.log(name);
+
+function add(a,b) {
+    const addBoth = a+b;
+}
+
+console.log(add(2,2));
+
+let arr = [10,20,30,40,50];
+delete arr[2];
+console.log(arr[2]);
+
+function sum(a,b,c) {
+   console.log(a,b,c);
+}
+sum(2,2);
+
+const student ={
+    name :'Arafat Rayhan',
+    Roll :1,
+    marks :99
+}
+console.log(student.marks);
