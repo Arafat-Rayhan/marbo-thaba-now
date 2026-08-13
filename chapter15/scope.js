2717be117134019cdb2854db49a979fbd6ffc4ad
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-13 15:43:16 +0600

    scope use

---
+++ chapter15/scope.js
@@ -0,0 +1,28 @@
+const taxRate = 10;
+function tax(params) {
+    const taxMoney = (params / 100) * taxRate; 
+    return taxMoney
+}
+
+console.log(tax(40000));
+
+function insite(params) {
+    let inscret = 'internal secret hiding place'
+}
+//console.log(inscret);
+
+if(taxRate===44){
+    let tampral = 45;
+}
+//console.log(tampral);
+
+function schoolDetails() {
+    const schoolName = 'A,K school';
+    function displayschoolName() {
+        console.log(schoolName);
+    }
+    return displayschoolName;
+}
+ const mySchool = schoolDetails();
+
+ 
