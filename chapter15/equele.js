1e0e5464ece2d6f8d3bda3c1b960700a7491fab7
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-13 12:16:46 +0600

    equele

---
+++ chapter15/equele.js
@@ -0,0 +1,56 @@
+if(1==1){
+    console.log('same');
+}
+
+const obj1 ={
+    name : 'Rayhan',
+    age :22,
+}
+
+const obj2 ={
+    name: 'Arafat',
+    age :25,
+}
+
+if(obj1===obj2){
+    console.log('same');
+}else{
+    console.log('diffarance');
+}
+
+const arr =[1,2,3,4,5];
+const arr2 = arr;
+if(arr === arr2){
+    console.log('same');    
+}else{
+    console.log('deffarance');
+}
+
+function cake(a,bolean) {
+    if(a==bolean){
+        console.log('same');
+    }else{
+        console.log('not');
+    }
+}
+cake(1,true)
+
+let y ='';
+if(y==false){
+    console.log('same');
+}else{
+    console.log('not');
+}
+
+if(null===undefined){
+    console.log('same');
+}else{
+    console.log('not');
+}
+
+if(1=='1'){
+    console.log('same');
+}else{
+    console.log('not');
+}
+
